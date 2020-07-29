93292a54d702ac51c93175c85a7f23653d4e7b5e
Zin Win Naing <zinwinnaing@achromex.com>
2020-07-29 13:45:09 +0630

    fixed in form

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,13 @@ const Home = () => {
       </Head>
 
       <main className={styles.main}>
-        <form name="contact" method="POST" data-netlify="true" netlify="true">
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          netlify="true"
+          action="/"
+        >
           <p>
             <label>
               Your Name: <input type="text" name="name" />
