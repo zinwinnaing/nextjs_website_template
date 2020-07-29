050e4cd8b8f051df7aff9a7ab06a7cea5e994025
Zin Win Naing <zinwinnaing@achromex.com>
2020-07-29 14:56:51 +0630

    add toml file

--- pages/index.js
+++ pages/index.js
@@ -1,98 +1,58 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-const handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-export default class Home extends React.Component {
-  state = {
-    name: "",
-    email: "",
-    message: "",
-  };
+      <main className={styles.main}>
+        <form name="contact" method="POST" data-netlify="true" netlify="true">
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Role:{" "}
+              <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select>
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </main>
 
-  encode = (data) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  };
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+};
 
-  handleSubmit = (e) => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "getintouch", ...this.state }),
-    })
-      .then(() => alert("Success!"))
-      .catch((error) => alert(error));
-
-    e.preventDefault();
-  };
-
-  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
-
-  render() {
-    const { name, email, message } = this.state;
-
-    return (
-      <div className={styles.container}>
-        <Head>
-          <title>Create Next App</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <main className={styles.main}>
-          <form onSubmit={this.handleSubmit}>
-            <p>
-              <label>
-                Your Name:{" "}
-                <input
-                  type="text"
-                  name="name"
-                  value={name}
-                  onChange={this.handleChange}
-                />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Email:{" "}
-                <input
-                  type="email"
-                  name="email"
-                  value={email}
-                  onChange={this.handleChange}
-                />
-              </label>
-            </p>
-            <p>
-              <label>
-                Message:{" "}
-                <textarea
-                  name="message"
-                  value={message}
-                  onChange={this.handleChange}
-                />
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-        </main>
-
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{" "}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-          </a>
-        </footer>
-      </div>
-    );
-  }
-}
+export default Home;
