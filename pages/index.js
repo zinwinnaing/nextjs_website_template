c379088b44df64071d6ac4d860ad25fd76980b88
Zin Win Naing <zinwinnaing@achromex.com>
2020-07-29 12:19:47 +0630

    fixed in index

--- pages/index.js
+++ pages/index.js
@@ -10,29 +10,15 @@ const Home = () => {
       </Head>
 
       <main className={styles.main}>
-        <form name="contact" method="POST" data-netlify="true">
+        <form name="contact" netlify>
           <p>
             <label>
-              Your Name: <input type="text" name="name" />
+              Name <input type="text" name="name" />
             </label>
           </p>
           <p>
             <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Role:{" "}
-              <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-              </select>
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message"></textarea>
+              Email <input type="email" name="email" />
             </label>
           </p>
           <p>
